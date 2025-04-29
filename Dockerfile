7facfa430269884d45b17e5ad743ab1fe189844b
Julia Alexeeva <julia4406@gmail.com>
2025-04-29 22:04:45 +0300

    Modify celery settings. Unsuccessful. Fixation commit. Celery-beat don't work.

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY app/ app/
-COPY celery_app.ry .
+COPY celery_app.py .
 
 CMD ["celery", "-A", "celery_app.celery", "worker", "--loglevel=info"]
--- celery_app.py
+++ celery_app.py
@@ -4,11 +4,12 @@ from celery import Celery
 
 celery = Celery(
     "jobs_checker",
-    broker=os.getenv("CELERY_BROKER_URL", "redis://redis:6381/0"),
-    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6381/1"),
+    broker=os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0"),
+    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/1"),
 )
 
 celery.conf.timezone = "UTC"
+celery.conf.beat_scheduler = "celery.beat.RedisScheduler"
 celery.conf.beat_schedule = {
     "check_job_every_hour": {
         "task": "app.tasks.check_new_jobs",
