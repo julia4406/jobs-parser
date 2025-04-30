16b873333c3b6614a77b77179cdda19fb052ae3f
Julia Alexeeva <julia4406@gmail.com>
2025-04-30 15:09:33 +0300

    Modify logging: now logs saves in volume in container

--- app/logging_settings.py
+++ app/logging_settings.py
@@ -1,5 +1,12 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
+
+
+log_directory = "/bot/logs"
+os.makedirs(log_directory, exist_ok=True)
+
+log_path = os.path.join(log_directory, "jobs_telegram.log")
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -7,7 +14,7 @@ log_format = "%(asctime)s - %(levelname)s - %(message)s"
 formatter = logging.Formatter(log_format)
 
 file_handler = RotatingFileHandler(
-    "jobs_telegram.log",
+    log_path,
     maxBytes=4_000,
     backupCount=1
 )
