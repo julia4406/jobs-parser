2bfc365343e7404cb1cdad4646f9f8a246ce8749
Julia Alexeeva <julia4406@gmail.com>
2025-05-07 16:21:09 +0300

    Modify urls structure

--- app/urls.py
+++ app/urls.py
@@ -6,19 +6,19 @@ URL_DOU_1_3_YEARS = f"{DOU_BASE}/vacancies/?category=Python&exp=1-3"
 
 
 LINKS_DOU = {
-    "first_job": URL_DOU_FIRST_JOB,
-    "under_1year": URL_DOU_UNDER_1_YEAR,
-    "1-3 years": URL_DOU_1_3_YEARS
+    "first_job_DOU": URL_DOU_FIRST_JOB,
+    "under_1year_exp_DOU": URL_DOU_UNDER_1_YEAR,
+    "1-3 years_exp_DOU": URL_DOU_1_3_YEARS
 }
 
 # DJINNI
-DJINNI_BASE = "https://djinni.co/jobs"
-URL_DJINNI_FIRST_JOB = f"{DJINNI_BASE}/?all_keywords=Python&exp_level=no_exp"
-URL_DJINNI_UNDER_1_YEAR = f"{DJINNI_BASE}/?all_keywords=Python&exp_level=1y"
-URL_DJINNI_2_YEARS = f"{DJINNI_BASE}/?all_keywords=Python&exp_level=2y"
+DJINNI_BASE = "https://djinni.co"
+URL_DJINNI_FIRST_JOB = f"{DJINNI_BASE}/jobs/?all_keywords=Python&exp_level=no_exp"
+URL_DJINNI_UNDER_1_YEAR = f"{DJINNI_BASE}/jobs/?all_keywords=Python&exp_level=1y"
+URL_DJINNI_2_YEARS = f"{DJINNI_BASE}/jobs/?all_keywords=Python&exp_level=2y"
 
 LINKS_DJINNI = {
-    "first_job": URL_DJINNI_FIRST_JOB,
-    "under_1year": URL_DJINNI_UNDER_1_YEAR,
-    "2 years": URL_DJINNI_2_YEARS
+    "first_job_DJINNI": URL_DJINNI_FIRST_JOB,
+    "under_1year_exp_DJINNI": URL_DJINNI_UNDER_1_YEAR,
+    "2 years_exp_DJINNI": URL_DJINNI_2_YEARS
 }
